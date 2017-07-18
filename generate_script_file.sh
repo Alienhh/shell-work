eead89ac915b826b5030d4c21eaec80db69cf212
alienhh <hhgg2009@qq.com>
2017-07-18 15:09:27 +0800

    update

--- generate_script_file.sh
+++ generate_script_file.sh
@@ -1,8 +1,14 @@
 #!/bin/zsh
-#产生脚本文件
+#Desc: generate shell script file 
+#Author: Alienhh
+#Date: 18/07/17
+#Version: 1.0.0
+function printUsage() {
+    echo "usage: ./generage_shell_script.sh [filename]"
+}
 script_file_name=$1
 if [ "$script_file_name" = "" ]; then
-	echo "file name can't null"
+    printUsage
 else
 	touch $script_file_name.sh
 	chmod +x  $script_file_name.sh
